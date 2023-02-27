9511cb9cc49dc0d2239127ab30745436b5d34129
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-02-27 20:09:07 +0900

    image viewer

--- ch07/src/main/webapp/image-viewer.js
+++ ch07/src/main/webapp/image-viewer.js
@@ -1,14 +1,34 @@
-imageViewer = {
+var imageViewer = {
 	init: function() {
-		for(var i=0; i<100; i++){
-			this._changeImage();
-		}
+		//$('.image-viewer .buttons #btn-change').click(imageViewer._changeImage);
+		$(imageViewer._init);
+	},
+	_init: function() {
+		$('.image-viewer #btn-change').click(imageViewer._changeImage);
+		$('.image-viewer #btn-slideshow').click(imageViewer._slideImage);
 	},
 	_changeImage: function() {
-		var index = Math.floor(Math.random() * this._images.length);
-		console.log(index);
-		console.log(this._images[index]);
+		var index = Math.floor(Math.random() * imageViewer._images.length);
+		var image = imageViewer._images[index];
+		$("img").replaceWith("<img src='images/"+image.file+"' title='"+ image.name+"' />");
+		imageViewer._imageIndex = index;
 	},
+	_slideImage: function() {
+		if(imageViewer._isSlide){
+			$('#btn-slideshow').html('<button id="btn-slideshow">슬라이드쇼  시작</button>');
+			clearInterval(imageViewer._intervalId);
+		} else {
+			$('#btn-slideshow').html('<button id="btn-slideshow">슬라이드쇼 종료</button>');
+			imageViewer._intervalId = setInterval(function() {
+				var image = imageViewer._images[imageViewer._imageIndex];
+				$("img").replaceWith("<img src='images/"+image.file+"' title='"+ image.name+"' />");
+				imageViewer._imageIndex =(imageViewer._imageIndex == (imageViewer._images.length-1)) ? 0 : imageViewer._imageIndex+1;
+			}, 1000);
+		}
+		imageViewer._isSlide = !imageViewer._isSlide;
+	},
+	_imageIndex: 6,
+	_isSlide : false,
 	_intervalId: null,
 	_images: [{
 			name: '국화',
@@ -35,4 +55,4 @@ imageViewer = {
 			name: '튤립',
 			file: 'Tulips.jpg'
 		}]
-};
+}
